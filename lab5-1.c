40565c89f7b1f68b3a94fd026a01bdc577697271
Sergdenis73 <sergdenis1973@gmail.com>
2025-04-23 22:13:00 +0300

    Final

--- lab5-1.c
+++ lab5-1.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 
-#define ROWS 3
-#define COLS 4
-
+#define N 4  
 int main() {
     system("chcp 65001");
-    double A[ROWS][COLS] = {
+    double A[N][N] = {
         {1.5, -2.3, 4.0, 0.0},
         {-1.7, 3.2, 5.1, -8.4},
-        {0.5, 2.7, -3.1, 6.9}
+        {0.5, 2.7, -3.1, 6.9},
+        {1.1, -0.8, 2.5, -4.4}
     };
     
-    printf("Матриця A:\n");
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLS; j++) {
+    printf("Квадратна матриця A (%dx%d):\n", N, N);
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
             printf("%6.2f ", A[i][j]);
         }
         printf("\n");
     }
     printf("\n");
     
-    for (int i = 0; i < ROWS; i++) {
+    for (int i = 0; i < N; i++) {
         int positive_count = 0;
-        for (int j = 0; j < COLS; j++) {
+        for (int j = 0; j < N; j++) {
             if (A[i][j] > 0) {
                 positive_count++;
             }
